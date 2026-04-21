ef53f0267d26282c43295b00670b21d8fbbc33f3
yonyroyer10 <yonyroyer10@gmail.com>
2026-04-21 13:46:20 -0500

    mover credenciales de supabase a archivo env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://neaisihwekincxzgnuwm.supabase.co'
-const supabaseAnonKey = 'sb_publishable_SaNhMarUS3_oHSiFTKf80w_mL57ulKh'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
